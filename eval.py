a7f71a0a161a5c87f50aa83e86877f4cb79efe59
Abdur Rahman <a.mohammed@42.guru>
2024-03-09 16:38:53 -0500

    not update model state

--- eval.py
+++ eval.py
@@ -276,8 +276,8 @@ if __name__ == "__main__":
 
     pth_path = "../swiftnet_davis_1_04_M_2400000.pth"
     state_dict = torch.load(pth_path)
-    new_state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-    model.load_state_dict(new_state_dict)#, strict=False)
+    # new_state_dict = {k.replace("", ""): v for k, v in state_dict.items()}
+    model.load_state_dict(state_dict)#, strict=False)
 
     metric = ["J", "F"]
     evaluate(model, Testloader, metric, 0)
